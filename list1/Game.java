94d442a64624c9549c1cb2fe74af1ca235970150
agnieszkakurzajewska <agnieszkakurzajewska@hotmail.com>
2021-11-01 23:55:52 +0100

    space as empty field

--- list1/Game.java
+++ list1/Game.java
@@ -5,8 +5,7 @@ class Game{
     public static void main(String args[]){   
 
         int board_side = 4;
-        List<Integer> permutation = generate_permutation(board_side);
-        int[][] board = fill_board(board_side, permutation);
+        int[][] board = fill_board(board_side, generate_permutation(board_side));
         print_board(board, board_side);
 
     }  
@@ -15,11 +14,11 @@ class Game{
 
         List<Integer> permutation = new ArrayList<>();
 
-        for (int i = 0; i < Math.pow(board_side, 2); i++) {
+        for (int i = 1; i < Math.pow(board_side, 2); i++) {
             permutation.add(i);
         }
+        Collections.shuffle(permutation);
 
-        Collections.shuffle(permutation); 
         return permutation; 
     }
 
@@ -32,8 +31,9 @@ class Game{
             for (int j=0; j<board_side; j++) {
                 if (i == board_side-1 && j == board_side-1) {
                     board[i][j] = 0;
+                } else {
+                    board[i][j] = permutation.get(permutation_counter++);
                 }
-                board[i][j] = permutation.get(permutation_counter++);
             }
         }
 
@@ -47,7 +47,11 @@ class Game{
         for (int i=0; i<board_side; i++) {
 
             for (int j=0; j<board_side; j++) {
-                System.out.printf("[%02d] ", board[i][j]);
+                if(board[i][j] == 0) {
+                    System.out.printf("[__] ");
+                } else {
+                    System.out.printf("[%02d] ", board[i][j]);
+                }        
             }
             System.out.println("\n");
 
